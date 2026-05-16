691697308cdeb6618bd4a146f573187ebea2e64a
Neeraja <24f1000470@ds.study.iitm.ac.in>
2026-05-16 22:46:16 +0530

    New Commit

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ app.config.from_object(Config)
 UPLOAD_FOLDER = "uploads/resumes"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 login_manager = LoginManager()
 login_manager.login_view = "login"
